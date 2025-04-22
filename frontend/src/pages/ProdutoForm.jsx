66e5ad1e8d545924b23733e874ce21713a6d2adc
Luiz_Hansen <luizhansen751@gmail.com>
2025-04-22 15:39:25 -0300

    mudanças

--- frontend/src/pages/ProdutoForm.jsx
+++ frontend/src/pages/ProdutoForm.jsx
@@ -16,8 +16,8 @@ const ProdutoForm = () => {
         console.log("Dados do produto:", data);
     };
     return (
-        <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ backgroundColor: '#ADD8E6', padding: 2, borderRadius: 1, mt: 2 }}>
-            <Toolbar sx={{ backgroundColor: '#ADD8E6', padding: 1, borderRadius: 2, mb: 2, display: 'flex', justifyContent: 'space-between' }}>
+        <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ backgroundColor: '#f01b1b', padding: 2, borderRadius: 1, mt: 2 }}>
+            <Toolbar sx={{ backgroundColor: '#f01b1b', padding: 1, borderRadius: 2, mb: 2, display: 'flex', justifyContent: 'space-between' }}>
                 <Typography variant="h6" color="primary">Dados Produto</Typography>
             </Toolbar>
             <Box sx={{ backgroundColor: 'white', padding: 2, borderRadius: 3, mb: 2 }}></Box>
